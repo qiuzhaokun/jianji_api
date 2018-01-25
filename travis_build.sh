0b9ff3bfbfd89d5f73108cbbcb55aab70a7f5914
Larry <837787527@qq.com>
2018-01-25 15:20:03 +0800

    生产环境配置文件

--- travis_build.sh
+++ travis_build.sh
@@ -4,5 +4,6 @@ git pull origin master
 chown -R nginx:nginx /webroot/api.jianji.myexist.cn/
 chmod -R 755 /webroot/api.jianji.myexist.cn/
 mv /webroot/api.jianji.myexist.cn/.env_prod /webroot/api.jianji.myexist.cn/.env
-cp -R /webroot/config/jianji_config /webroot/api.jianji.myexist.cn/config
+rm -rf /webroot/api.jianji.myexist.cn/config
+cp /webroot/config/jianji_config /webroot/api.jianji.myexist.cn/config
 echo 'travis build done!'
