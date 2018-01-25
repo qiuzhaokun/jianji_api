43d6907c383fa1cc3cd299b161512efa261d0fe2
Larry <837787527@qq.com>
2018-01-25 14:43:04 +0800

    init project

--- travis_build.sh
+++ travis_build.sh
@@ -3,4 +3,6 @@ cd /webroot/api.jianji.myexist.cn/
 git pull origin master
 chown -R nginx:nginx /webroot/api.jianji.myexist.cn/
 chmod -R 755 /webroot/api.jianji.myexist.cn/
+mv /webroot/api.jianji.myexist.cn/.env_prod /webroot/api.jianji.myexist.cn/.env
+mv /webroot/api.jianji.myexist.cn/config_prd /webroot/api.jianji.myexist.cn/config
 echo 'travis build done!'
